5b17a6591cc096f96f3d3348fdfbc2daee9da910
Fang <felord@qq.com>
2022-03-28 15:51:42 +0800

    private_key_jwt

--- oauth2-clients/src/main/java/cn/felord/oauth2/wechat/WechatOAuth2AuthorizationCodeGrantRequestEntityConverter.java
+++ oauth2-clients/src/main/java/cn/felord/oauth2/wechat/WechatOAuth2AuthorizationCodeGrantRequestEntityConverter.java
@@ -18,34 +18,18 @@ package cn.felord.oauth2.wechat;
 
 import cn.felord.oauth2.config.JwkResolver;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.security.oauth2.client.endpoint.NimbusJwtClientAuthenticationParametersConverter;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequestEntityConverter;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationExchange;
 import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
-import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
-import org.springframework.security.oauth2.jwt.JwsHeader;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Map;
 
 /**
  * 兼容微信请求参数的请求参数封装工具类,扩展了{@link OAuth2AuthorizationCodeGrantRequestEntityConverter}
@@ -59,10 +43,8 @@ import java.util.Map;
  */
 public class WechatOAuth2AuthorizationCodeGrantRequestEntityConverter
         implements Converter<OAuth2AuthorizationCodeGrantRequest, RequestEntity<?>> {
-    private static final HttpHeaders DEFAULT_TOKEN_REQUEST_HEADERS = getDefaultTokenRequestHeaders();
     private static final String WECHAT_ID = "wechat";
     private final OAuth2AuthorizationCodeGrantRequestEntityConverter defaultConverter = new OAuth2AuthorizationCodeGrantRequestEntityConverter();
-    private JwtEncoder jwtEncoder;
 
 
     /**
@@ -86,32 +68,6 @@ public class WechatOAuth2AuthorizationCodeGrantRequestEntityConverter
         }
     }
 
-
-    private String generateClientAssertion(ClientRegistration clientRegistration) {
-        JwsHeader.Builder jwsHeaderBuilder = JwsHeader.with(SignatureAlgorithm.RS256);
-        Instant issuedAt = Instant.now();
-        Instant expiresAt = issuedAt.plus(Duration.ofMinutes(1));
-        String clientId = clientRegistration.getClientId();
-        ClientRegistration.ProviderDetails providerDetails = clientRegistration.getProviderDetails();
-        Map<String, Object> configurationMetadata = providerDetails.getConfigurationMetadata();
-        String revocationEndpoint = (String) configurationMetadata.get("revocation_endpoint");
-        String introspectionEndpoint = (String) configurationMetadata.get("introspection_endpoint");
-        JwtClaimsSet.Builder claimsBuilder = JwtClaimsSet.builder()
-                // oidc  定义
-                .issuer(clientId)
-                // oidc 定义
-                .subject(clientId)
-                .audience(Arrays.asList(providerDetails.getTokenUri(),
-                        introspectionEndpoint,
-                        revocationEndpoint))
-                .issuedAt(issuedAt)
-                .expiresAt(expiresAt)
-                .notBefore(issuedAt)
-                .claim(OAuth2ParameterNames.SCOPE, Collections.emptyMap());
-        return jwtEncoder.encode(JwtEncoderParameters.from(jwsHeaderBuilder.build(), claimsBuilder.build())).getTokenValue();
-    }
-
-
     private MultiValueMap<String, String> buildWechatQueryParameters(OAuth2AuthorizationCodeGrantRequest authorizationCodeGrantRequest) {
         // 获取微信的客户端配置
         ClientRegistration clientRegistration = authorizationCodeGrantRequest.getClientRegistration();
@@ -131,35 +87,5 @@ public class WechatOAuth2AuthorizationCodeGrantRequestEntityConverter
         //secret
         formParameters.add("secret", clientRegistration.getClientSecret());
         return formParameters;
-    }
-
-
-    static HttpHeaders getTokenRequestHeaders(ClientRegistration clientRegistration) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.addAll(DEFAULT_TOKEN_REQUEST_HEADERS);
-        if (ClientAuthenticationMethod.CLIENT_SECRET_BASIC.equals(clientRegistration.getClientAuthenticationMethod())
-                || ClientAuthenticationMethod.BASIC.equals(clientRegistration.getClientAuthenticationMethod())) {
-            String clientId = encodeClientCredential(clientRegistration.getClientId());
-            String clientSecret = encodeClientCredential(clientRegistration.getClientSecret());
-            headers.setBasicAuth(clientId, clientSecret);
-        }
-        return headers;
-    }
-
-    private static String encodeClientCredential(String clientCredential) {
-        try {
-            return URLEncoder.encode(clientCredential, StandardCharsets.UTF_8.toString());
-        } catch (UnsupportedEncodingException ex) {
-            // Will not happen since UTF-8 is a standard charset
-            throw new IllegalArgumentException(ex);
-        }
-    }
-
-    private static HttpHeaders getDefaultTokenRequestHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON_UTF8));
-        final MediaType contentType = MediaType.valueOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE + ";charset=UTF-8");
-        headers.setContentType(contentType);
-        return headers;
     }
 }
