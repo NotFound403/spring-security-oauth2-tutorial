69b227fcee481abe09fad8c44064c67ac688a4db
Fang <felord@qq.com>
2022-04-11 15:22:50 +0800

    client secret jwt

--- gateway/src/main/java/cn/felord/gateway/security/ReactiveWebSecurityConfiguration.java
+++ gateway/src/main/java/cn/felord/gateway/security/ReactiveWebSecurityConfiguration.java
@@ -1,10 +1,22 @@
 package cn.felord.gateway.security;
 
+import com.nimbusds.jose.jwk.OctetSequenceKey;
+import com.nimbusds.oauth2.sdk.auth.ClientAuthenticationMethod;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.endpoint.NimbusJwtClientAuthenticationParametersConverter;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.endpoint.ReactiveOAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.WebClientReactiveAuthorizationCodeTokenResponseClient;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-//@EnableWebFluxSecurity
+
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+@EnableWebFluxSecurity
 public class ReactiveWebSecurityConfiguration {
     @Bean
     @ConditionalOnMissingBean
@@ -13,5 +25,24 @@ public class ReactiveWebSecurityConfiguration {
         http.oauth2Login();
         http.oauth2Client();
         return http.build();
+    }
+
+    @Bean
+    public ReactiveOAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> oAuth2AccessTokenResponseClient() {
+
+        NimbusJwtClientAuthenticationParametersConverter<OAuth2AuthorizationCodeGrantRequest> clientAuthenticationParametersConverter = new NimbusJwtClientAuthenticationParametersConverter<>(clientRegistration -> {
+            if (ClientAuthenticationMethod.CLIENT_SECRET_JWT.equals(clientRegistration.getClientAuthenticationMethod())) {
+                SecretKeySpec secretKey = new SecretKeySpec(
+                        clientRegistration.getClientSecret().getBytes(StandardCharsets.UTF_8),
+                        "HmacSHA256");
+                return new OctetSequenceKey.Builder(secretKey)
+                        .keyID(UUID.randomUUID().toString())
+                        .build();
+            }
+            return null;
+        });
+        WebClientReactiveAuthorizationCodeTokenResponseClient tokenResponseClient = new WebClientReactiveAuthorizationCodeTokenResponseClient();
+        tokenResponseClient.addParametersConverter(clientAuthenticationParametersConverter);
+        return tokenResponseClient;
     }
 }
